10525fef3ab18abb21912f18dcd34ed5392cb91f
Ravikiran Basavaraj <ravikiran.basavaraj@in.fujitsu.com>
2017-04-17 13:07:47 +0530

    First Commit

---
+++ unittest/test_01.py
@@ -0,0 +1,12 @@
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        print('Basic Unit Test case demo devops')
+        print('Basic Unit1 Test case demo devops')
+        print('Basic Unit Test case demo devops')
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
